865f8609e992ba05902224abfc343a5e69d9b969
Théo HUET <theo6huet@gmail.com>
2025-03-24 15:12:25 +0100

    Update harmonic-oscillator.py

--- harmonic-oscillator.py
+++ harmonic-oscillator.py
@@ -16,7 +16,8 @@ TEMPS = 10
 
 #condition initiale (ici sous forme de loi normal):
 SIGMA = 1
-X0 = 2
+X0 = 1.5
+K0 = 2 # quantité de mouvement initial : p0 = hbar k0
 
 #constante de numérisation :
 Nx = 100 #nombre de |x>
@@ -28,7 +29,7 @@ dt = TEMPS/Nt
 mesure_interval = 100
 
 #pour l'aniamtion :
-animation_interval = 25  #Intervalle pour l'animation (tout les combiens de step on sauvegarde les positions)
+animation_interval = 20  #Intervalle pour l'animation (tout les combiens de step on sauvegarde les positions)
 save_animation = False #si on sauvegarde l'animation sur la machine
 save_frames = True  #si on fait une annimation
 
@@ -37,52 +38,60 @@ save_frames = True  #si on fait une annimation
 def convert(seconds):
     return time.strftime("%H:%M:%S", time.gmtime(execution_time))
 
-def animate_trajectory(positions_for_animation, L): #Animation des trajectoires de particule
-    fig, ax = plt.subplots(figsize=(6,6))
-    scat = ax.scatter([], [], s=20) #, color='blue'
-    ax.set_xlim(-L/2, L/2)
-    ax.set_ylim(-L/2, L/2)
-    title = "Harmonic Oscillator"
-    ax.set_title(title)
-    plt.ylim([0,1])
-    plt.xlim([-L/2,L/2])
+def animate_trajectory(probability_for_animation,energy_evolution, Vx, L): #Animation des trajectoires de particule
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+    scat = ax2.scatter([], [], s=20, color ='blue')
+    ax1.plot(x, Vx, color = 'g', label="V(x)",  linestyle = '--')
+    ax1.plot(x, energy_evolution, color = 'red', label="<H>")
+    
+    ax1.set_xlim(-L/2, L/2)
+    ax2.set_ylim(0, 1.1)
+    
+    ax1.set_xlabel('x')
+    ax1.set_ylabel("V(x) & <H>", color = 'green')
+    ax1.set_ylabel("<H>", color = 'red')
+    ax2.set_ylabel('|ψ|²', color='blue')
+    
+    title = "Harmonic Oscilator"
+    ax1.set_title(title)
+
     def init():
         scat.set_offsets(np.empty((0, 2)))
         return (scat,)
     
     def update(frame_index):
-        coords = positions_for_animation[frame_index]
+        coords = probability_for_animation[frame_index]
         scat.set_offsets(coords)
-        ax.set_title(f"Frame {frame_index}/{len(positions_for_animation)-1}")
+        ax1.set_title(f"Frame {frame_index}/{len(probability_for_animation)-1}")
         return (scat,)
     
-    #print("Shape for animation:", positions_for_animation.shape)
+    #print("Shape for animation:", probability_for_animation.shape)
     
     ani = animation.FuncAnimation(
         fig, update,
-        frames=len(positions_for_animation),
+        frames=len(probability_for_animation),
         init_func=init,
         blit=True,
         interval=50 #50ms -> 20fps
     )
     if save_animation :
-        ani.save("harmonic-oscillator.mp4", writer="ffmpeg", fps=20) #pour sauvegarder l'animation en .mp4 avec ffmpeg
+        ani.save("harmonic-oscilator.mp4", writer="ffmpeg", fps=20) #pour sauvegarder l'animation en .mp4 avec ffmpeg
         plt.close(fig)
     else :
         plt.show()
 
-def compute_energy(psi, H):
+def compute_energy(psi, H): #mesure de l'energie moyenne
     return np.real(np.vdot(psi, H @ psi))  # <psi|H|psi>
 
-def compute_U(H) :
+def compute_U(H) : #opérateur evolution
     factor = 1j * H * dt / (2* HBAR)
     I = np.eye(Nx)
     num = I - factor
     den = I + factor
     return num, den
 
-#potentiel
-def potential(x) :
+def potential(x) : # calcul le potentiel
     return x * x
 
 ############################### main ###############################
@@ -90,18 +99,16 @@ def potential(x) :
 start_time = time.perf_counter()
 
 #initialisation
-positions_for_animation = []
+probability_for_animation = []
 
 x = np.arange(-L/2, L/2, dx)
 
-if True :
-    k0 = 0  # quantité de mouvement initial : p0 = hbar k0
-    psi0 = np.exp(-0.5 * (x - X0)**2 / SIGMA**2) * np.exp(1j * k0 * x)
+if True : #paquet d'onde
+    psi0 = np.exp(-0.5 * (x - X0)**2 / SIGMA**2) * np.exp(1j * K0 * x)
     psi0 /= np.linalg.norm(psi0)
-else :
+else : #un etat propre donc un |x> avec x = X0
     psi0 = np.zeros(Nx)
     psi0[int((X0+(L/2))*(Nx/L))] = 1
-
 #Compute Opérateur évolution
 Vx = potential(x) #V(x)
 V = np.diag(Vx) #matrice diagonale du potentiel dans la base des |x>
@@ -116,13 +123,13 @@ U_numerateur, U_denominateur = compute_U(H) # U l'opérateur évolution = U_nume
 energy_evolution = []
 mesure_time = []
 E_moy = compute_energy(psi0, H)
-print("energie = ",E_moy)
+print("energie initiale = ", E_moy)
 
 #compute
 psi = psi0
 for n in range(Nt) :
     #resoud la PDE
-    psi = solve(U_numerateur,U_denominateur @ psi) #np.matmul(U, psi)
+    psi = solve(U_numerateur, U_denominateur @ psi) # U_d|psi_n+1> = U_n|psi_n>
     
     # mesure des moyennes d'énérgie
     E_inst = compute_energy(psi, H)
@@ -140,11 +147,11 @@ for n in range(Nt) :
         positions = []
         for i in range(Nx) :
             positions.append([i*dx - L/2, np.abs(psi[i])**2])
-        positions_for_animation.append(positions)
+        probability_for_animation.append(positions)
         #print(f"image {n/animation_interval}/{Nt/animation_interval}")
     
     normalisation = np.sum(np.abs(psi)**2)
-    if np.abs(1 - normalisation) > 0.01 :
+    if np.abs(1 - normalisation) > 0.001 :
         print(f"Resultat diverge l'état n'est plus normalisé : Somme des |psi|² = {normalisation}")
         break
 
@@ -154,14 +161,5 @@ execution_time = end_time - start_time
 print(f"Programme exécuté en : {convert(execution_time)}")
 
 # lance l'animation
-if save_frames and len(positions_for_animation) > 1 :
-    animate_trajectory(positions_for_animation, L)
-    
-# affiche les courbes de mesures
-plt.plot(mesure_time[1:], energy_evolution[1:])
-plt.ylim(min(energy_evolution[1:])-1,max(energy_evolution)+1)
-plt.xlim(0,max(mesure_time))
-plt.xlabel("Temps")
-plt.ylabel("Energy")
-plt.title("Évolution de l'Energie")
-plt.show()
+if save_frames and len(probability_for_animation) > 1 :
+    animate_trajectory(probability_for_animation,[np.mean(energy_evolution[1:])]*Nx, Vx, L)
