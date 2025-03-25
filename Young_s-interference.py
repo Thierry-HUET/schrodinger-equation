7c4ac382c0bdac6ef65507ba536c4f93f7bc176e
TheoHuetQC <theo6huet@gmail.com>
2025-03-25 19:36:02 +0100

    Update Young_s-interference.py

--- Young_s-interference.py
+++ Young_s-interference.py
@@ -12,7 +12,7 @@ M = 1
 
 #constante du problème :
 L = 10
-TEMPS = 100
+TEMPS = 1
 
 #condition initiale (ici sous forme de loi normal):
 SIGMAx = 1 #écart type de la fonction d'onde initial
@@ -28,9 +28,9 @@ PERIODE_V = 2 #tout les combiens de distance on a une barriere
 V0 = 10 #La hauteur du potentiel
 
 #constante de numérisation :
-Nx = 200 #nombre de |x>
-Ny = 200
-Nt = 20000 #nombre de pas de temps
+Nx = 50 #nombre de |x>
+Ny = 50
+Nt = 800 #nombre de pas de temps
 dx = L/Nx
 dy = L/Ny
 dt = TEMPS/Nt
@@ -39,7 +39,7 @@ dt = TEMPS/Nt
 mesure_interval = 100
 
 #pour l'aniamtion :
-animation_interval = 20  #Intervalle pour l'animation (tout les combiens de step on sauvegarde les positions)
+animation_interval = 5  #Intervalle pour l'animation (tout les combiens de step on sauvegarde les positions)
 save_animation = False #si on sauvegarde l'animation sur la machine
 save_frames = True  #si on fait une annimation
 
@@ -48,55 +48,24 @@ save_frames = True  #si on fait une annimation
 def convert(seconds):
     return time.strftime("%H:%M:%S", time.gmtime(execution_time))
 
-def animate_trajectory(probability_for_animation,energy_evolution, Vx, L): #Animation des trajectoires de particule
-    fig, ax1 = plt.subplots()
-    ax2 = ax1.twinx()
-    scat = ax2.scatter([], [], s=20, color ='blue')
-    ax1.plot(x, Vx, color = 'g', label="V(x)",  linestyle = '--')
-    ax1.plot(x, energy_evolution, color = 'red', label="<H>")
-    
-    ax1.set_xlim(-L/2, L/2)
-    ax2.set_ylim(0, 1.1)
-    
-    ax1.set_xlabel('x')
-    ax1.set_ylabel("V(x) & <H>", color = 'green')
-    ax1.set_ylabel("<H>", color = 'red')
-    ax2.set_ylabel('|ψ|²', color='blue')
-    
-    title = "Tunneling effect"
-    ax1.set_title(title)
+def animate_wavefunction_2D(probability_for_animation):
+    fig, ax = plt.subplots()
+    im = ax.imshow(probability_for_animation[0], cmap="inferno", extent=[-L/2, L/2, -L/2, L/2], origin="lower")
 
-    def init():
-        scat.set_offsets(np.empty((0, 2)))
-        return (scat,)
-    
     def update(frame_index):
-        coords = probability_for_animation[frame_index]
-        scat.set_offsets(coords)
-        ax1.set_title(f"{title} Frame {frame_index}/{len(probability_for_animation)-1}")
-        return (scat,)
-    
-    #print("Shape for animation:", probability_for_animation.shape)
-    
-    ani = animation.FuncAnimation(
-        fig, update,
-        frames=len(probability_for_animation),
-        init_func=init,
-        blit=True,
-        interval=50 #50ms -> 20fps
-    )
-    if save_animation :
-        ani.save("tunneling-effect-2D.mp4", writer="ffmpeg", fps=20) #pour sauvegarder l'animation en .mp4 avec ffmpeg
-        plt.close(fig)
-    else :
-        plt.show()
+        im.set_array(probability_for_animation[frame_index])
+        return im,
+
+    ani = animation.FuncAnimation(fig, update, frames=len(probability_for_animation), interval=50)
+    plt.colorbar(im, label="|ψ|²")
+    plt.show()
 
 def compute_energy(psi, H): #mesure de l'energie moyenne
     return np.real(np.vdot(psi, H @ psi))  # <psi|H|psi>
 
 def compute_U(H) : #opérateur evolution
     factor = 1j * H * dt / (2* HBAR)
-    I = np.eye(Nx)
+    I = np.eye(Nx * Ny)
     num = I - factor
     den = I + factor
     return num, den
@@ -111,40 +80,37 @@ start_time = time.perf_counter()
 # initialisation
 probability_for_animation = []
 
-x, y = np.meshgrid(np.linspace(-L/2, L/2, Nx), np.linspace(-L/2, L/2, Ny)) # on fait une grille 2D
+X, Y = np.meshgrid(np.linspace(-L/2, L/2, Nx), np.linspace(-L/2, L/2, Ny)) # on fait une grille 2D
+# paquet d'onde
+psi0 = np.exp(-0.5 * ((X - X0)**2 / SIGMAx**2 + (Y - Y0)**2 / SIGMAy**2)) * np.exp(1j * (Kx0 * X + Ky0 * Y))
+psi0 /= np.linalg.norm(psi0)
 
-if True : # paquet d'onde
-    psi0 = [np.exp(-0.5 * (x - X0)**2 / SIGMAx**2) * np.exp(1j * Kx0 * x), np.exp(-0.5 * (y - Y0)**2 / SIGMAy**2) * np.exp(1j * Ky0 * y)]
-    psi0 /= np.linalg.norm(psi0)
-else : # un etat propre donc un |x> tensoriel |y> avec x = X0 et y = YO
-    psi0 = np.zeros(Nx,Ny)
-    psi0[int((X0+(L/2))*(Nx/L)), int((Y0+(L/2))*(Ny/L))] = 1
 #Compute Opérateur évolution
-Vxy = potential(x, y) # V(x, y)
+Vxy = potential(X, Y) # V(x, y)
 V = np.diag(Vxy.flatten()) # matrice diagonale du potentiel dans la base des |x> tensoriel |y>
 
 LaplacienX = (np.eye(Nx, k=1) - 2*np.eye(Nx) + np.eye(Nx, k=-1)) / dx**2
 LaplacienY = (np.eye(Ny, k=1) - 2*np.eye(Ny) + np.eye(Ny, k=-1)) / dy**2
 Laplacien = np.kron(LaplacienX, np.eye(Ny)) + np.kron(np.eye(Nx), LaplacienY) # np.kron = produit tensoriel
 
-H = - HBAR*HBAR * Laplacien / (2*M) + V # l'Hamiltonien dans la base des |x> tensoriel |y>
+H = - (HBAR*HBAR * Laplacien / (2*M)) + V # l'Hamiltonien dans la base des |x> tensoriel |y>
 
 U_numerateur, U_denominateur = compute_U(H) # U l'opérateur évolution = U_numerateur / U_denominateur 
 
 # init des mesures
 energy_evolution = []
 mesure_time = []
-E_moy = compute_energy(psi0, H)
-print("energie initiale = ", E_moy)
+"""E_moy = compute_energy(psi0, H)
+print("energie initiale = ", E_moy)"""
 
 # compute
-psi = psi0
+psi = psi0.flatten()
 for n in range(Nt) :
     # resoud la PDE
     psi = solve(U_numerateur, U_denominateur @ psi) # U_d|psi_n+1> = U_n|psi_n>
     
     # mesure des moyennes d'énérgie
-    E_inst = compute_energy(psi, H)
+    """E_inst = compute_energy(psi, H)
     E_moy += E_inst
     if (n % mesure_interval == 0) :
         # mesure de l'énergie
@@ -153,27 +119,34 @@ for n in range(Nt) :
         E_moy = 0
 
         # temps ou on a fait la mesure
-        mesure_time.append(n*dt)
+        mesure_time.append(n*dt)"""
         
     if n % animation_interval  == 0 : #on sauvegarde les positions pour les annimées tout les "animation_interval" pas 
-        positions = []
-        for i in range(Nx) :
-            positions.append([i*dx - L/2, np.abs(psi[i])**2])
-        probability_for_animation.append(positions)
-        #print(f"image {n/animation_interval}/{Nt/animation_interval}")
+        probability_for_animation.append(np.abs(psi.reshape(Nx, Ny))**2)
+        print(f"image {n/animation_interval}/{Nt/animation_interval}")
     
-    normalisation = np.sum(np.abs(psi)**2)
+    """normalisation = np.sum(np.abs(psi.reshape(Nx, Ny))**2)
     if np.abs(1 - normalisation) > 0.001 :
         print(f"Resultat diverge l'état n'est plus normalisé : Somme des |psi|² = {normalisation}")
-        break
+        break"""
 
-print("energie finale = ", energy_evolution[-1])
+#print("energie finale = ", energy_evolution[-1])
 
 # temps que met le programme
 end_time = time.perf_counter()
 execution_time = end_time - start_time
 print(f"Programme exécuté en : {convert(execution_time)}")
 
+# plot le potentiel en 3D
+fig = plt.figure(figsize=(8,6))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Y, Vxy, cmap='plasma')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('V(x, y)')
+ax.set_title('Potentiel en 2D')
+plt.show()
+
 # lance l'animation
 if save_frames and len(probability_for_animation) > 1 :
-    animate_trajectory(probability_for_animation,[np.mean(energy_evolution[1:])]*Nx, Vx, L)
+    animate_wavefunction_2D(probability_for_animation)
